8bf0103b2cc6bf19fa5aa9ce5541a61e14a3a4bc
Miguel <mares112358@gmail.com>
2019-01-30 09:27:12 -0600

    Actualizacion para continuar con el blog

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 var btnMenu = document.getElementById("btn-menu");
 var nav = document.getElementById("nav");
 
-btnMenu.addEventListener('click', function(){
-	nav.classList.toggle('mostrar');
-})
+btnMenu.addEventListener("click", function () {
+    nav.classList.toggle("mostrar");
+});
